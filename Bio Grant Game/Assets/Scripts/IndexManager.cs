e9eda3c9f2d9ca02e83fb80558765a0d88fc7e44
KingWapo <Holcombea9172@my.uwstout.edu>
2015-04-02 20:07:58 -0500

    Setting up the index page

---
+++ Bio Grant Game/Assets/Scripts/IndexManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IndexManager : MonoBehaviour {
+
+    public GameObject DirectoryView;
+    public GameObject InfoView;
+
+    private bool inDirectoryView;
+
+	// Use this for initialization
+	void Start () {
+        inDirectoryView = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
