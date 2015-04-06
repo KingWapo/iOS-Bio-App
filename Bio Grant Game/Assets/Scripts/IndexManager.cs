588817cb691e434ec40a6a054b51a6eeebb6d035
KingWapo <Holcombea9172@my.uwstout.edu>
2015-04-06 11:45:27 -0500

    Setting up Index page

--- Bio Grant Game/Assets/Scripts/IndexManager.cs
+++ Bio Grant Game/Assets/Scripts/IndexManager.cs
@@ -1,20 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class IndexManager : MonoBehaviour {
 
+    public GameObject PlantParent;
     public GameObject DirectoryView;
+    public GameObject DirectoryPlantPositions;
+    public GameObject DirectoryTextPositions;
     public GameObject InfoView;
 
     private bool inDirectoryView;
+    private int page;
+    private int plantsPerPage;
+    private List<GameObject> plants;
 
 	// Use this for initialization
 	void Start () {
         inDirectoryView = true;
+        page = 0;
+        plantsPerPage = 12;
+        plants = new List<GameObject>();
+        for (int i = 0; i < PlantParent.transform.childCount; i++)
+        {
+            plants.Add(PlantParent.transform.GetChild(i).gameObject);
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    public void Next()
+    {
+        page++;
+        setPlants();
+    }
+
+    public void Previous()
+    {
+        page--;
+        setPlants();
+    }
+
+    private void setPlants()
+    {
+        for (int i = page * plantsPerPage; i < (page + 1) * plantsPerPage || i < plants.Count; i++)
+        {
+
+        }
+    }
 }
